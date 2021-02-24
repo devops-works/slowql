b53931c546adad78b88490db55218332488755cf
eze-kiel <ezekiel@fastmail.com>
2021-02-24 17:41:40 +0100

    added header for documentation

--- slowql.go
+++ slowql.go
@@ -1,3 +1,7 @@
+// Package slowql provides everything needed to parse slow query logs from
+// different databases (such as MySQL, MariaDB).
+// Along to a parser, it proposes a simple API with few functions that allow
+// you to get everything needed to compute your slow queries.
 package slowql
 
 import (
